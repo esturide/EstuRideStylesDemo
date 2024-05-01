26f6f00d492ad6188da83356163ba0d8e53a086d
SealtielFreak <Die.Seal_Valderrama@outlook.com>
2024-04-30 19:00:20 -0600

    Typescript implemented and reformat structure

--- App.tsx
+++ App.tsx
@@ -1,16 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Main from './src/screens/Main';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
-      <StatusBar style="auto" />
+    <View style={style.container}>
+      <Main/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
---
+++ src/screens/Main.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function Main() {
+    return (
+      <>
+        <Text>Hello world from main screen!</Text>
+        <StatusBar style="auto" />
+      </>
+    );
+  }
