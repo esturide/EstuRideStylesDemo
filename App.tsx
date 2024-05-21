ac6766a12358cd1c677e9ce27c99b8e56d3ccf77
DiegoRuiz8 <diego.ruiz4125@alumnos.udg.mx>
2024-05-20 20:30:16 -0600

    createFirstStyles

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Main from './src/screens/Main';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import Main from "./src/screens/Main";
+import Button from "./src/components/Button";
+import styles from "./src/styles/GlobalStyles";
+import BtnSubmit from "./src/components/BtnSubmit";
+import BtnDepositarVerde from "./src/components/BtnDepositarVerde";
+import BtnDepositarAzul from "./src/components/BtnDepositarAzul";
+import BtnRetirar from "./src/components/BtnRetirar";
+
 
 export default function App() {
   return (
-    <View style={style.container}>
-      <Main/>
+    <View style={styles.container}>
+      <Text style={styles.title}>Componentes</Text>
+      <Button title="Press me!" onPress={() => alert("Button Pressed!")} />
+      <BtnSubmit title="Label"/>
+      <BtnDepositarVerde/>
+      <BtnRetirar/>
+      <BtnDepositarAzul/>
     </View>
+    
   );
 }
-
-const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
