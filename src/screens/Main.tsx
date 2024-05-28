914a25d9132ba54fa8ef610312f935f89020dda8
Gesem-Martinez <109618325+Gesem-Martinez@users.noreply.github.com>
2024-05-27 21:24:52 -0600

    Add components to display ride info

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,13 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import CardVehicleInfo from "../components/CardVehicleInfo";
+import CardViajes from "../components/CardViajes";
+import CardViajesInfoPasajero from "../components/CardViajesInfoPasajero";
 
 export default function Main() {
   return (
     <>
       <Text>Hello world from main screen!</Text>
       <StatusBar style="auto" />
-      <CardVehicleInfo/>
+      <View style={{rowGap: 20}}>
+        <CardVehicleInfo />
+        <CardViajes />
+        <CardViajesInfoPasajero />
+      </View>
     </>
   );
 }
