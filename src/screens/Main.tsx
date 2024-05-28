2facc3fb63497d87827020f59ce9e026aeb86a7e
Gesem-Martinez <109618325+Gesem-Martinez@users.noreply.github.com>
2024-05-27 23:41:47 -0600

    Add components to display posts and community posts

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,8 +1,27 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Text, View, FlatList } from "react-native";
 import CardVehicleInfo from "../components/CardVehicleInfo";
 import CardViajes from "../components/CardViajes";
 import CardViajesInfoPasajero from "../components/CardViajesInfoPasajero";
+import CardPost from "../components/CardPost";
+import CommunityPostItem from "../components/CommunityPostItem";
+
+const DATA = [
+  {
+    id: '01',
+    readStatus: true,
+    communityName: "Cut",
+    postTime: "9:40 AM",
+    postBody: "Hola comunidad, este espacio es para que nos dejen sus mensajes",
+  },
+  {
+    id: '02',
+    readStatus: false,
+    communityName: "Futbol",
+    postTime: "11:00 AM",
+    postBody: "Hoy se arman las retas a las 3:00",
+  }
+]
 
 export default function Main() {
   return (
@@ -13,6 +32,8 @@ export default function Main() {
         <CardVehicleInfo />
         <CardViajes />
         <CardViajesInfoPasajero />
+        <CardPost/>
+        <FlatList data={DATA} renderItem={({item}) => <CommunityPostItem readStatus={item.readStatus} communityName={item.communityName} postTime={item.postTime} postBody={item.postBody}/>}/>
       </View>
     </>
   );
