14188a3b0df3a53be60a59fe64b97f187f12b95a
Gesem-Martinez <109618325+Gesem-Martinez@users.noreply.github.com>
2024-06-03 10:58:26 -0600

    Add custom radio group component and credit card radio group component

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,40 +1,64 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View, FlatList } from "react-native";
+import { ScrollView, Text, View, FlatList } from "react-native";
 import CardVehicleInfo from "../components/CardVehicleInfo";
 import CardViajes from "../components/CardViajes";
 import CardViajesInfoPasajero from "../components/CardViajesInfoPasajero";
 import CardPost from "../components/CardPost";
 import CommunityPostItem from "../components/CommunityPostItem";
+import CreditCardList from "../components/CreditCardList";
 
 const DATA = [
   {
-    id: '01',
+    id: "01",
     readStatus: true,
     communityName: "Cut",
     postTime: "9:40 AM",
     postBody: "Hola comunidad, este espacio es para que nos dejen sus mensajes",
   },
   {
-    id: '02',
+    id: "02",
     readStatus: false,
     communityName: "Futbol",
     postTime: "11:00 AM",
     postBody: "Hoy se arman las retas a las 3:00",
-  }
-]
+  },
+];
 
 export default function Main() {
   return (
     <>
       <Text>Hello world from main screen!</Text>
       <StatusBar style="auto" />
-      <View style={{rowGap: 20}}>
-        <CardVehicleInfo />
-        <CardViajes />
-        <CardViajesInfoPasajero />
-        <CardPost/>
-        <FlatList data={DATA} renderItem={({item}) => <CommunityPostItem readStatus={item.readStatus} communityName={item.communityName} postTime={item.postTime} postBody={item.postBody}/>}/>
-      </View>
+      <ScrollView>
+        <View style={{ rowGap: 20 }}>
+          <CardVehicleInfo />
+          <CardViajes />
+          <CardViajesInfoPasajero />
+          <CardPost />
+          <FlatList
+            ItemSeparatorComponent={() => (
+              <View
+                style={{
+                  height: 0.5,
+                  width: 312,
+                  alignSelf: "flex-end",
+                  backgroundColor: "#3C3C435C",
+                }}
+              />
+            )}
+            data={DATA}
+            renderItem={({ item }) => (
+              <CommunityPostItem
+                readStatus={item.readStatus}
+                communityName={item.communityName}
+                postTime={item.postTime}
+                postBody={item.postBody}
+              />
+            )}
+          />
+          <CreditCardList />
+        </View>
+      </ScrollView>
     </>
   );
 }
