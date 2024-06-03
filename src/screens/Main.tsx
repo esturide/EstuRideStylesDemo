d067d7164f88c32fdaa619598a4cea8467a4500c
Gesem-Martinez <109618325+Gesem-Martinez@users.noreply.github.com>
2024-06-03 11:05:04 -0600

    Change main screen to original state

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,64 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { ScrollView, Text, View, FlatList } from "react-native";
-import CardVehicleInfo from "../components/CardVehicleInfo";
-import CardViajes from "../components/CardViajes";
-import CardViajesInfoPasajero from "../components/CardViajesInfoPasajero";
-import CardPost from "../components/CardPost";
-import CommunityPostItem from "../components/CommunityPostItem";
-import CreditCardList from "../components/CreditCardList";
-
-const DATA = [
-  {
-    id: "01",
-    readStatus: true,
-    communityName: "Cut",
-    postTime: "9:40 AM",
-    postBody: "Hola comunidad, este espacio es para que nos dejen sus mensajes",
-  },
-  {
-    id: "02",
-    readStatus: false,
-    communityName: "Futbol",
-    postTime: "11:00 AM",
-    postBody: "Hoy se arman las retas a las 3:00",
-  },
-];
+import { Text, View } from "react-native";
 
 export default function Main() {
   return (
     <>
       <Text>Hello world from main screen!</Text>
       <StatusBar style="auto" />
-      <ScrollView>
-        <View style={{ rowGap: 20 }}>
-          <CardVehicleInfo />
-          <CardViajes />
-          <CardViajesInfoPasajero />
-          <CardPost />
-          <FlatList
-            ItemSeparatorComponent={() => (
-              <View
-                style={{
-                  height: 0.5,
-                  width: 312,
-                  alignSelf: "flex-end",
-                  backgroundColor: "#3C3C435C",
-                }}
-              />
-            )}
-            data={DATA}
-            renderItem={({ item }) => (
-              <CommunityPostItem
-                readStatus={item.readStatus}
-                communityName={item.communityName}
-                postTime={item.postTime}
-                postBody={item.postBody}
-              />
-            )}
-          />
-          <CreditCardList />
-        </View>
-      </ScrollView>
     </>
   );
 }
